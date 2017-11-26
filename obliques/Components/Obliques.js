bd55a32e6c13df24eae1066513d158d49be8f15f
Tom Carlson <tccarlson04@gmail.com>
2017-11-26 12:57:47 -0700

    consolidated the multiple variables into one object that I can traverse and push to an array

--- obliques/Components/Obliques.js
+++ obliques/Components/Obliques.js
@@ -1,539 +1,150 @@
 import React, { Component } from 'react';
 import { Text, Image, View, StyleSheet } from 'react-native';
 
-let 1 = {
-	strategy: "Remove specifics and convert to ambiguities"
+
+let strategies = {
+	strategy1: "Remove specifics and convert to ambiguities",
+	strategy2: "Think of the radio",
+	strategy3: "Don't be frightened of clichés",
+	strategy4: "Allow an easement (easement: abandonment of a stricture)"
+	strategy5: "What is the reality of the situation?",
+	strategy6: "Simple subtraction",
+	strategy7: "Are there sections? Consider transitions",
+	strategy8: "Turn it upside down",
+	strategy9: "Go slowly all the way round the outside",
+	strategy10: "A line has two sides",
+	strategy11: "Infinitesimal gradations",
+	strategy12: "Make an exhaustive list of everything you might do and do the last thing on the list",
+	strategy13: "Change instrument roles",
+	strategy14: "Into the impossible",
+	strategy15: "Accretion: gradual accumulation of additional layers",
+	strategy16: "Ask people to work against their better judgment",
+	strategy17: "Disconnect from desire",
+	strategy18: "Take away the elements in order of apparent non-importance",
+	strategy19: "Emphasize repetitions",
+	strategy20: "Change instrument roles",
+	strategy21: "Don't be afraid of things becuase they are easy to do",
+	strategy22: "Is there something missing?",
+	strategy23: "Don't be frightened to display your talents",
+	strategy24: "Use unqualified people",
+	strategy25: "Breathe more deeply",
+	strategy26: "How would you have done it?",
+	strategy27: "Honor thy error as a hidden intention :)",
+	strategy28: "Emphasize differences",
+	strategy29: "Only one element of each kind",
+	strategy30: "Do nothing for as long as possible",
+	strategy31: "Bridges: build | burn",
+	strategy32: "Water",
+	strategy33: "You don't have to be ashamed of using your own ideas",
+	strategy34: "Make a sudden, destructive, unpredictable action; incorporate",
+	strategy35: "Tidy up",
+	strategy36: "Consult other sources",
+	strategy37: "Do the words need changing?",
+	strategy38: "Use an acceptable color",
+	strategy39: "Ask your body",
+	strategy40: "Humanize something free of error",
+	strategy41: "Use filters",
+	strategy42: "Balance the consistency principle with the inconsistency principle",
+	strategy43: "Fill every beat with something",
+	strategy44: "Discard an axiom",
+	strategy45: "Listen to the quiet voice",
+	strategy46: "What wouldn't you do?",
+	strategy47: "Is it finished?",
+	strategy48: "Decorate, decorate",
+	strategy49: "Put in earplugs",
+	strategy50: "Give the game away",
+	strategy51: "Reverse",
+	strategy52: "Abandon normal instruments",
+	strategy53: "Trust in the you of now :)",
+	strategy54: "Use fewer notes",
+	strategy55: "What would your closest friend do?",
+	strategy56: "Repetition is a form of change",
+	strategy57: "Distorting time",
+	strategy58: "Give way to your worst impulse",
+	strategy59: "Make a blank valuable by putting it in an exquisite frame"
+	strategy60: "",
+	strategy61: "The inconsistency principle",
+	strategy62: "Ghost echoes",
+	strategy63: "Don't break the silence",
+	strategy64: "You can only make one dot at a time",
+	strategy65: "Discover the recipes you are using and abandon them",
+	strategy66: "Just carry on",
+	strategy67: "Cascades",
+	strategy68: "(Organic) machinery",
+	strategy69: "Courage!",
+	strategy70: "What mistakes did you make last time?",
+	strategy71: "You are an engineer",
+	strategy72: "Consider different fading systems",
+	strategy73: "Remove ambiguities and convert to specifics",
+	strategy74: "Mute and continue",
+	strategy75: "Look at the order in which you do things",
+	strategy76: "It is quite possible (after all)",
+	strategy77: "Go outside. Shut the door",
+	strategy78: "Don't stress on thing more than another",
+	strategy79: "Do we need holes?",
+	strategy80: "Clear analysis",
+	strategy81: "Work at a different speed",
+	strategy82: "Do something boring",
+	strategy83: "Look closely at the most embarrassing details and amplify them",
+	strategy84: "Define an area as 'safe' and use it as an anchor",
+	strategy85: "Mechanicalize something idiosyncratic",
+	strategy86: "Overtly resist change",
+	strategy87: "Emphasize the flaws",
+	strategy88: "Accept advice",
+	strategy89: "Remember those quiet evenings",
+	strategy90: "Take a break",
+	strategy91: "The tape is now the music",
+	strategy92: "Short circuit (example: a man eating peas with the idea that they will improve his virility shovels them straight into his lap)",
+	strategy93: "Imagine the music as a moving chain or caterpillar",
+	strategy94: "Use an old idea",
+	strategy95: "Intentions: credibility of | nobility of | humility of",
+	strategy96: "Destroy: nothing | the most important thing",
+	strategy97: "Imagine the music as a set of disconnected events",
+	strategy98: "Change nothing and continue with immaculate consistency",
+	strategy99: "What are you really thinking about just now? Incorporate",
+	strategy100: "Children's voices: speaking | singing",
+	strategy101: "Assemble some of the instruments in a group and treat the group",
+	strategy102: "Feedback recordings into an acoustic situation",
+	strategy103: "Shut the door and listen from outside",
+	strategy104: "Towards the insignificant",
+	strategy105: "Is the tuning appropriate?",
+	strategy106: "Simply a matter of work",
+	strategy107: "Look at a very small object, look at its center",
+	strategy108: "Not building a wall but making a brick",
+	strategy109: "Revaluation (a warm feeling)",
+	strategy110: "Disciplined self-indulgence",
+	strategy111: "The most important thing is the thing most easily forgotten",
+	strategy112: "Always first steps",
+	strategy113: "Idiot glee",
+	strategy114: "Question the heoric approach",
+	strategy115: "Be EXTRAvagant",
+	strategy116: "Always give yourself credit for having more than personality",
+	strategy117: "State the problem in words as clearly as possible",
+	strategy118: "Faced with a choice, do both",
+	strategy119: "Tape your mouth",
+	strategy120: "Twist the spine",
+	strategy121: "Get your neck massaged",
+	strategy122: "Lowest common denominator check: single beat | single note | single riff",
+	strategy123: "Do the washing up",
+	strategy124: "Listen in total darkness or in a very large room, very quietly",
+	strategy125: "Convert a melodic element into a rythmic element",
+	strategy126: "Would anybody want it?",
+	strategy127: "Spectrum analysis",
+	strategy128: "Retrace your steps",
+	strategy129: "Go to an extreme, move back to a more comfortable place",
+	strategy130: "Once the search is in progress, something will be found",
+	strategy131: "Only a part, not the whole",
+	strategy132: "From nothing to more than nothing",
+	strategy133: "Be less critical more often",
 };
 
-let 2 = {
-	strategy: "Think of the radio"
-};
+let strategyArray = [];
 
-let 3 = {
-	strategy: "Don't be frightened of clichés"
-};
-
-let 4 = {
-	strategy: "Allow an easement (easement: abandonment of a stricture)"
-};
-
-let 5 = {
-	strategy: "What is the reality of the situation?"
-};
-
-let 6 = {
-	strategy: "Simple subtraction"
-};
-
-let 7 = {
-	strategy: "Are there sections? Consider transitions"
-};
-
-let 8 = {
-	strategy: "Turn it upside down"
-};
-
-let 9 = {
-	strategy: "Go slowly all the way round the outside"
-};
-
-let 10 = {
-	strategy: "A line has two sides"
-};
-
-let 11 = {
-	strategy: "Infinitesimal gradations"
-};
-
-let 12 = {
-	strategy: "Make an exhaustive list of everything you might do and do the last thing on the list"
-};
-
-let 13 = {
-	strategy: "Change instrument roles"
-};
-
-let 14 = {
-	strategy: "Into the impossible"
-};
-
-let 15 = {
-	strategy: "Accretion: gradual accumulation of additional layers"
-};
-
-let 16 = {
-	strategy: "Ask people to work against their better judgment"
-};
-
-let 17 = {
-	strategy: "Disconnect from desire"
-};
-
-let 18 = {
-	strategy: "Take away the elements in order of apparent non-importance"
-};
-
-let 19 = {
-	strategy: "Emphasize repetitions"
-};
-
-let 20 = {
-	strategy: "Change instrument roles"
-};
-
-let 21 = {
-	strategy: "Don't be afraid of things becuase they are easy to do"
-};
-
-let 22 = {
-	strategy: "Is there something missing?"
-};
-
-let 23 = {
-	strategy: "Don't be frightened to display your talents"
-};
-
-let 24 = {
-	strategy: "Use unqualified people"
-};
-
-let 25 = {
-	strategy: "Breathe more deeply"
-};
-
-let 26 = {
-	strategy: "How would you have done it?"
-};
-
-let 27 = {
-	strategy: "Honor thy error as a hidden intention :)"
-};
-
-let 28 = {
-	strategy: "Emphasize differences"
-};
-
-let 29 = {
-	strategy: "Only one element of each kind"
-};
-
-let 30 = {
-	strategy: "Do nothing for as long as possible"
-};
-
-let 31 = {
-	strategy: "Bridges: build | burn"
-};
-
-let 32 = {
-	strategy: "Water"
-};
-
-let 33 = {
-	strategy: "You don't have to be ashamed of using your own ideas"
-};
-
-let 34 = {
-	strategy: "Make a sudden, destructive, unpredictable action; incorporate"
-};
-
-let 35 = {
-	strategy: "Tidy up"
-};
-
-let 36 = {
-	strategy: "Consult other sources"
-};
-
-let 37 {
-	strategy: "Do the words need changing?"
+for (var prop in obj) {
+	if(obj.hasOwnProperty(prop)) {
+		strategyArray.push(prop);
+	}
 }
-
-let 38 = {
-	strategy: "Use an acceptable color"
-};
-
-let 39 = {
-	strategy: "Ask your body"
-};
-
-let 40 = {
-	strategy: "Humanize something free of error"
-};
-
-let 41 = {
-	strategy: "Use filters"
-};
-
-let 42 = {
-	strategy: "Balance the consistency principle with the inconsistency principle"
-};
-
-let 43 = {
-	strategy: "Fill every beat with something"
-};
-
-let 44 = {
-	strategy: "Discard an axiom"
-};
-
-let 45 = {
-	strategy: "Listen to the quiet voice"
-};
-
-let 46 = {
-	strategy: "What wouldn't you do?"
-};
-
-let 47 = {
-	strategy: "Is it finished?"
-};
-
-let 48 = {
-	strategy: "Decorate, decorate"
-};
-
-let 49 = {
-	strategy: "Put in earplugs"
-};
-
-let 50 = {
-	strategy: "Give the game away"
-};
-
-let 51 = {
-	strategy: "Reverse"
-};
-
-let 52 = {
-	strategy: "Abandon normal instruments"
-};
-
-let 53 = {
-	strategy: "Trust in the you of now :)"
-};
-
-let 54 = {
-	strategy: "Use fewer notes"
-};
-
-let 55 = {
-	strategy: "What would your closest friend do?"
-};
-
-let 56 = {
-	strategy: "Repetition is a form of change"
-};
-
-let 57 = {
-	strategy: "Distorting time"
-};
-
-let 58 = {
-	strategy: "Give way to your worst impulse"
-};
-
-let 59 = {
-	strategy: "Make a blank valuable by putting it in an exquisite frame"
-};
-
-let 60 = {
-	strategy: ""
-};
-
-let 61 {
-	strategy: "The inconsistency principle"
-};
-
-let 62 = {
-	strategy: "Ghost echoes"
-};
-
-let 63 = {
-	strategy: "Don't break the silence"
-};
-
-let 64 = {
-	strategy: "You can only make one dot at a time"
-};
-
-let 65 = {
-	strategy: "Discover the recipes you are using and abandon them"
-};
-
-let 66 = {
-	strategy: "Just carry on"
-};
-
-let 67 = {
-	strategy: "Cascades"
-};
-
-let 68 = {
-	strategy: "(Organic) machinery"
-};
-
-let 69 = {
-	strategy: "Courage!"
-};
-
-let 70 = {
-	strategy: "What mistakes did you make last time?"
-};
-
-let 71 = {
-	strategy: "You are an engineer"
-};
-
-let 72 = {
-	strategy: "Consider different fading systems"
-};
-
-let 73 = {
-	strategy: "Remove ambiguities and convert to specifics"
-};
-
-let 74 = {
-	strategy: "Mute and continue"
-};
-
-let 75 = {
-	strategy: "Look at the order in which you do things"
-};
-
-let 76 = {
-	strategy: "It is quite possible (after all)"
-};
-
-let 77 = {
-	strategy: "Go outside. Shut the door"
-};
-
-let 78 = {
-	strategy: "Don't stress on thing more than another"
-};
-
-let 79 = {
-	strategy: "Do we need holes?"
-};
-
-let 80 = {
-	strategy: "Clear analysis"
-};
-
-let 81 = {
-	strategy: "Work at a different speed"
-};
-
-let 82 = {
-	strategy: "Do something boring"
-};
-
-let 83 = {
-	strategy: "Look closely at the most embarrassing details and amplify them"
-};
-
-let 84 = {
-	strategy: "Define an area as 'safe' and use it as an anchor"
-};
-
-let 85 = {
-	strategy: "Mechanicalize something idiosyncratic"
-};
-
-let 86 = {
-	strategy: "Overtly resist change"
-};
-
-let 87 = {
-	strategy: "Emphasize the flaws"
-};
-
-let 88 = {
-	strategy: "Accept advice"
-};
-
-let 89 = {
-	strategy: "Remember those quiet evenings"
-};
-
-let 90 = {
-	strategy: "Take a break"
-};
-
-let 91 = {
-	strategy: "The tape is now the music"
-};
-
-let 92 = {
-	strategy: "Short circuit (example: a man eating peas with the idea that they will improve his virility shovels them straight into his lap)"
-};
-
-let 93 = {
-	strategy: "Imagine the music as a moving chain or caterpillar"
-};
-
-let 94 = {
-	strategy: "Use an old idea"
-};
-
-let 95 = {
-	strategy: "Intentions: credibility of | nobility of | humility of"
-};
-
-let 96 = {
-	strategy: "Destroy: nothing | the most important thing"
-};
-
-let 97 = {
-	strategy: "Imagine the music as a set of disconnected events"
-};
-
-let 98 = {
-	strategy: "Change nothing and continue with immaculate consistency"
-};
-
-let 99 = {
-	strategy: "What are you really thinking about just now? Incorporate"
-};
-
-let 100 = {
-	strategy: "Children's voices: speaking | singing"
-};
-
-let 101 = {
-	strategy: "Assemble some of the instruments in a group and treat the group"
-};
-
-let 102 = {
-	strategy: "Feedback recordings into an acoustic situation"
-};
-
-let 103 = {
-	strategy: "Shut the door and listen from outside"
-};
-
-let 104 = {
-	strategy: "Towards the insignificant"
-};
-
-let 105 = {
-	strategy: "Is the tuning appropriate?"
-};
-
-let 106 = {
-	strategy: "Simply a matter of work"
-};
-
-let 107 = {
-	strategy: "Look at a very small object, look at its center"
-};
-
-let 108 = {
-	strategy: "Not building a wall but making a brick"
-};
-
-let 109 = {
-	strategy: "Revaluation (a warm feeling)"
-};
-
-let 110 = {
-	strategy: "Disciplined self-indulgence"
-};
-
-let 111 = {
-	strategy: "The most important thing is the thing most easily forgotten"
-};
-
-let 112 = {
-	strategy: "Always first steps"
-};
-
-let 113 = {
-	strategy: "Idiot glee"
-};
-
-let 114 = {
-	strategy: "Question the heoric approach"
-};
-
-let 115 = {
-	strategy: "Be EXTRAvagant"
-};
-
-let 116 = {
-	strategy: "Always give yourself credit for having more than personality"
-};
-
-let 117 = {
-	strategy: "State the problem in words as clearly as possible"
-};
-
-let 118 = {
-	strategy: "Faced with a choice, do both"
-};
-
-let 119 = {
-	strategy: "Tape your mouth"
-};
-
-let 120 = {
-	strategy: "Twist the spine"
-};
-
-let 121 = {
-	strategy: "Get your neck massaged"
-};
-
-let 122 = {
-	strategy: "Lowest common denominator check: single beat | single note | single riff"
-};
-
-let 123 = {
-	strategy: "Do the washing up"
-};
-
-let 124 = {
-	strategy: "Listen in total darkness or in a very large room, very quietly"
-};
-
-let 125 = {
-	strategy: "Convert a melodic element into a rythmic element"
-};
-
-let 126 = {
-	strategy: "Would anybody want it?"
-};
-
-let 127 = {
-	strategy: "Spectrum analysis"
-};
-
-let 128 = {
-	strategy: "Retrace your steps"
-};
-
-let 129 = {
-	strategy: "Go to an extreme, move back to a more comfortable place"
-};
-
-let 130 = {
-	strategy: "Once the search is in progress, something will be found"
-};
-
-let 131 = {
-	strategy: "Only a part, not the whole"
-};
-
-let 132 = {
-	strategy: "From nothing to more than nothing"
-};
-
-let 133 = {
-	strategy: "Be less critical more often" 
-};
-
-
 
 
 
