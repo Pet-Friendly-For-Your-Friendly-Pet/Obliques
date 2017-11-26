a989ec23e632e97fed60d885a8f5c05ff6a25ea5
Tom Carlson <tccarlson04@gmail.com>
2017-11-26 16:46:00 -0700

    basic functionality of random strategies is now up and running, now to figure out navigation!

--- obliques/App.js
+++ obliques/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, Image, View, StyleSheet, Dimensions, Platform, PixelRatio } from 'react-native';
-
+import { Text, Image, View, StyleSheet, Dimensions, Platform, PixelRatio, StackNavigator} from 'react-native';
 
 var _ = require('lodash');
 // import TestComponent from './Components/Test.js';
 import Obliques from './Components/Obliques';
+
+
 
 export default class App extends React.Component {
   render() {
@@ -16,6 +17,7 @@ export default class App extends React.Component {
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
